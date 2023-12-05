d5f5419a4630f79297d1d603f995027ba3add028
oumaima inaoui <oumaima-inaoui@hotmail.com>
2023-12-05 12:57:30 +0100

    added side bar

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ st.write("""
 Spaced Repetition System SQL practice
 """)
 
-#the select box
-option = st.selectbox(
-    "What would you like to review?",
-    ("Joins", "GroupBy" , "Windows Functions"),
-    index = None,
-    placeholder="Select a theme.."
-)
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review?",
+        ("Joins", "GroupBy" , "Windows Functions"),
+        index = None,
+        placeholder="Select a theme.."
+    )
 
-st.write('You selected:' , option)
+    st.write('You selected:' , option)
 
 
 data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
