8ce6a735f0df7916234355acccdcc135a5261c0a
Oumaima Inaoui <122205842+OumaimaI27@users.noreply.github.com>
2023-12-05 13:38:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ st.write("""
 Spaced Repetition System SQL practice
 """)
 
-with st.sidebar:
-    option = st.selectbox(
-        "What would you like to review?",
-        ("Joins", "GroupBy" , "Windows Functions"),
-        index = None,
-        placeholder="Select a theme.."
-    )
+option = st.selectbox(
+    "What would you like to review?",
+    ("Joins", "GroupBy" , "Windows Functions"),
+    index = None,
+    placeholder="Select a theme..")
 
-    st.write('You selected:' , option)
+
+st.write('You selected:' , option)
 
 
 data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
